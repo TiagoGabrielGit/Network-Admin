d520317c6a41f4fc54c1433e11c5680fd24c95ca
TiagoGabrielGit <tiago@gigafull.com.br>
2022-08-08 00:58:13 -0300

    v2.3.1

--- servicedesk/consultar_chamados/processa/add.php
+++ servicedesk/consultar_chamados/processa/add.php
@@ -10,8 +10,8 @@ $relato = $_POST['relatoChamado'];
 
 $cont_insert = false;
 
-$sql = "INSERT INTO chamados (atendente_id, assuntoChamado, tipochamado_id, solicitante_id, empresa_id, status_id, data_abertura, in_execution, in_execution_atd_id)
-        VALUES ('0', :assuntoChamado, :tipochamado_id, :solicitante_id, :empresa_id, '1', NOW(), '0', '0')";
+$sql = "INSERT INTO chamados (atendente_id, assuntoChamado, tipochamado_id, solicitante_id, empresa_id, status_id, data_abertura, in_execution, in_execution_atd_id, seconds_worked)
+        VALUES ('0', :assuntoChamado, :tipochamado_id, :solicitante_id, :empresa_id, '1', NOW(), '0', '0', '0')";
 $stmt1 = $pdo->prepare($sql);
 $stmt1->bindParam(':assuntoChamado', $assuntoChamado);
 $stmt1->bindParam(':tipochamado_id', $tipochamado_id);
@@ -26,8 +26,8 @@ if ($stmt1->execute()) {
 }
 
 if ($id_chamado && $cont_insert) {
-    $sql_relato = "INSERT INTO chamado_relato (chamado_id, relator_id, relato, relato_hora_inicial, relato_hora_final)
-    VALUES (:chamado_id, :relator_id, :relato, NOW(), NOW())";
+    $sql_relato = "INSERT INTO chamado_relato (chamado_id, relator_id, relato, relato_hora_inicial, relato_hora_final, seconds_worked)
+    VALUES (:chamado_id, :relator_id, :relato, NOW(), NOW(), '0')";
 
     $stmt2 = $pdo->prepare($sql_relato);
     $stmt2->bindParam(':chamado_id', $id_chamado);
